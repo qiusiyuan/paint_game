14330a28bdb857de9865043d7b220a14a9b512b9
Qiu Siyuan <truxihere@gmail.com>
2020-02-06 03:59:01 +0000

    paint

---
+++ src/App.js
@@ -0,0 +1,50 @@
+  
+import React, { Component } from 'react';
+import Paint from './containers/Paint';
+// import './App.css';
+// // import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Home from './containers/Home';
+// import Accounts from './containers/Accounts';
+// import Forms from './containers/Forms';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      page: "accounts",
+      id: null
+    };
+    this.switchPage = this.switchPage.bind(this);
+  }
+
+  switchPage(options){
+    this.setState(options);
+  }
+
+  render(){
+    // let mainComponent;
+    // switch(this.state.page){
+    //   case "home":
+    //     mainComponent = <Home switchPage={this.switchPage}/>;
+    //     break;
+    //   case "accounts":
+    //     mainComponent = <Accounts switchPage={this.switchPage}/>;
+    //     break;
+    //   case "forms":
+    //     mainComponent = <Forms switchPage={this.switchPage} match={{params:{accountID: this.state.id}}}/>;
+    //     break;
+    // }
+    
+    return (
+      <div id="mainapp">
+        <div className="container-fluid">
+          <p>hello</p>
+          <Paint/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
